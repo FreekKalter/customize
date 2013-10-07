e8c180be4156eda44871a1907abe786f1665e6f3
Freek Kalter <freek@kalteronline.org>
2013-10-07 21:30:28 +0200

    added git configs and comments

--- customize.sh
+++ customize.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+# get packages
 apt-get install git zsh tmux
+
+# git configs
+git config --global user.name "Freek Kalter"
+git config --global user.email "freek@kalteronline.org"
+git config --global core.edior vim
+git config --global core.pager less
+git config --global color.ui true
+git config --global color true
+
+# clone all repos
 cd ~
 git clone git@github.com:FreekKalter/zsh-conf zsh-conf
 git clone git@github.com:FreekKalter/vim .vim
@@ -7,11 +18,13 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 git clone https://github.com/clvv/fasd.git fasd
 cp ~/fasd/fasd /usr/bin/fasd
 
+# zsh and tmux config files
 cd ~/zsh-conf
 ./create_hooks.sh
 ./create_links.sh
-chsh -s zsh
+chsh -s /bin/zsh
 
+# vim submodule inints and .vimrc linking
 cd ~/.vim
 ./create_hooks.sh
 ./create_links.sh
